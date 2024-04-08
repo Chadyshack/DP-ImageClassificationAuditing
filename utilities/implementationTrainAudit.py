cf2733e7bb53681d1f1fc91c2790e6f9d6169e5e
Chad Schwenke <chad.schwenke@colostate.edu>
2024-04-07 22:29:17 -0600

    testing w augmentations

--- utilities/implementationTrainAudit.py
+++ utilities/implementationTrainAudit.py
@@ -28,15 +28,17 @@ def main(args):
     # Data transformations and loading
     data_transforms = {
         'train': torchvision.transforms.Compose([
-            torchvision.transforms.Resize(224),
+            torchvision.transforms.RandomResizedCrop(224),
+            torchvision.transforms.RandomHorizontalFlip(),
             torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         'test': torchvision.transforms.Compose([
-            torchvision.transforms.Resize(224),
+            torchvision.transforms.Resize(256),
+            torchvision.transforms.CenterCrop(224),
             torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ])
+            torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
     }
 
 ######################### TODO check start
